7536dedc7d209cc36319e84dafcbca1d654ad015
Sudhan007 <sudhancool87@gmail.com>
2024-03-18 18:43:14 +0530

    responsive-pages

--- client/src/components/InternShip/internShipImportant/Modal.svelte
+++ client/src/components/InternShip/internShipImportant/Modal.svelte
@@ -1,78 +1,87 @@
 <script lang="ts">
-	import Button from '$lib/components/ui/button/button.svelte';
-	import Icon from '@iconify/svelte';
+  import Button from "$lib/components/ui/button/button.svelte";
+  import Icon from "@iconify/svelte";
+  import { onMount } from "svelte";
 
-	export let showModal: boolean; // boolean
-	let dialog: HTMLDialogElement; // HTMLDialogElement
+  export let showModal: boolean; // boolean
+  let dialog: HTMLDialogElement; // HTMLDialogElement
 
-	$: {
-		if (dialog && showModal) {
-			dialog.showModal();
-			window.document.body.style.overflow = 'hidden';
-		} else {
-			window.document.body.style.overflow = '';
-		}
-	}
+  onMount(() => {
+    let scrolloff = (document.body.style.overflow = "hidden");
+  });
+
+  $: {
+    if (dialog && showModal) {
+      dialog.showModal();
+      // window.document.body.style.overflow = 'hidden';
+    } else {
+      // window.document.body.style.overflow = '';
+    }
+  }
 </script>
 
 <!-- svelte-ignore a11y-click-events-have-key-events a11y-no-noninteractive-element-interactions -->
 
 <dialog
-	class=" rounded-2xl bg-[#242424] p-2 opacity-90 shadow-2xl"
-	bind:this={dialog}
-	on:close={() => (showModal = false)}
-	on:click|self={() => dialog.close()}
+  class=" rounded-2xl bg-[#242424] p-2 opacity-90 shadow-2xl"
+  bind:this={dialog}
+  on:close={() => (showModal = false)}
+  on:click|self={() => dialog.close()}
 >
-	<!-- svelte-ignore a11y-no-static-element-interactions -->
-	<div on:click|stopPropagation>
-		<!-- svelte-ignore a11y-autofocus -->
-		<div class="flex justify-end bg-transparent">
-			<Button
-				class="opacity-95 hover:bg-transparent"
-				variant="ghost"
-				size="icon"
-				autofocus
-				on:click={() => dialog.close()}
-			>
-				<Icon icon="mingcute:close-fill" class="text-3xl" style="color: white" />
-			</Button>
-		</div>
+  <!-- svelte-ignore a11y-no-static-element-interactions -->
+  <div on:click|stopPropagation>
+    <!-- svelte-ignore a11y-autofocus -->
+    <div class="flex justify-end bg-transparent">
+      <Button
+        class="opacity-95 hover:bg-transparent"
+        variant="ghost"
+        size="icon"
+        autofocus
+        on:click={() => dialog.close()}
+      >
+        <Icon
+          icon="mingcute:close-fill"
+          class="text-3xl"
+          style="color: white"
+        />
+      </Button>
+    </div>
 
-		<slot name="header" />
-		<slot />
-	</div>
+    <slot name="header" />
+    <slot />
+  </div>
 </dialog>
 
 <style>
-	dialog {
-		border: none;
-	}
-	dialog::backdrop {
-		background: rgba(0, 0, 0, 0.3);
-	}
-	dialog > div {
-		padding: 1em;
-	}
-	dialog[open] {
-		animation: zoom 0.3s cubic-bezier(0.34, 1.56, 0.64, 1);
-	}
-	@keyframes zoom {
-		from {
-			transform: scale(0.95);
-		}
-		to {
-			transform: scale(1);
-		}
-	}
-	dialog[open]::backdrop {
-		animation: fade 0.2s ease-out;
-	}
-	@keyframes fade {
-		from {
-			opacity: 0;
-		}
-		to {
-			opacity: 1;
-		}
-	}
+  dialog {
+    border: none;
+  }
+  dialog::backdrop {
+    background: rgba(0, 0, 0, 0.3);
+  }
+  dialog > div {
+    padding: 1em;
+  }
+  dialog[open] {
+    animation: zoom 0.3s cubic-bezier(0.34, 1.56, 0.64, 1);
+  }
+  @keyframes zoom {
+    from {
+      transform: scale(0.95);
+    }
+    to {
+      transform: scale(1);
+    }
+  }
+  dialog[open]::backdrop {
+    animation: fade 0.2s ease-out;
+  }
+  @keyframes fade {
+    from {
+      opacity: 0;
+    }
+    to {
+      opacity: 1;
+    }
+  }
 </style>
